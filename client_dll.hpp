dbd067661366007c5e258b24d8d39c6796d2b830
ProjectTrinity1337 <Projecttrinity1337@gmail.com>
2026-08-11 15:32:54 +0100

    Update client_dll.hpp

--- client_dll.hpp
+++ client_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-11 14:26:58.930959500 UTC
+// 2026-08-11 02:15:46.596267700 UTC
 
 #pragma once
 
@@ -10,7 +10,7 @@ namespace cs2_dumper {
     namespace schemas {
         // Module: client.dll
         // Class count: 563
-        // Enum count: 16
+        // Enum count: 17
         namespace client_dll {
             // Alignment: 4
             // Member count: 5
